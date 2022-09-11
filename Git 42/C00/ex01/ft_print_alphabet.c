80a75a0e4fae3db1da84152a801b3da0f1e751b1
Andre Mealha Soares <amealha-@c2r7s4.42lausanne.ch>
2022-09-11 16:41:22 +0200

    backup v3

---
+++ Git 42/C00/ex01/ft_print_alphabet.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_alphabet.c                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: amealha- <marvin@42lausanne.ch>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/09/01 12:43:58 by amealha-          #+#    #+#             */
+/*   Updated: 2022/09/01 18:02:28 by amealha-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_print_alphabet(void)
+{
+	char	alpha;
+
+	alpha = 'a';
+	while (alpha <= 'z')
+	{
+		write(1, &alpha, 1);
+		alpha++;
+	}
+}
+/*int main(){
+	ft_print_alphabet();
+}*/
