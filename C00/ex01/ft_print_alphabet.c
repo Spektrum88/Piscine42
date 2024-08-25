a3200985fd0e5daf9568a228ffcd6ed74bc54100
Spektrum <andre_soares88@hotmail.com>
2024-08-25 22:47:46 +0200

    update42

---
+++ C00/ex01/ft_print_alphabet.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_alphabet.c                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: amealha- <marvin@42lausanne.ch>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/09/01 12:43:58 by amealha-          #+#    #+#             */
+/*   Updated: 2022/09/19 15:45:44 by amealha-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_print_alphabet(void)
+{
+	char	alpha;
+
+	alpha = 'a';
+	while (alpha <= 'z')
+	{
+		write(1, &alpha, 1);
+		alpha++;
+	}
+}
+int main(){
+	ft_print_alphabet();
+}
+
+
--- testesC/prototype/main.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-void proto();
-
-int main(void)
-{
-	proto(0);
-	return(0);
-}
